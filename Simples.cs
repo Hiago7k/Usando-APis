f11d0fd52cda4c6bbb6e75b786e48147c6b305bf
Hiago 7k <hiago7k@gmail.com>
2026-03-16 21:25:37 -0300

    novo desafio invocar try catch para objeto nulo

---
+++ Simples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usando_APis
+{
+    internal class Simples
+    {
+        public void Executar()
+        {
+            Console.WriteLine("Testando ");
+        }
+    }
+}

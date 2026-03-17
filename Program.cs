1e0c45f60f54e53ea220980d608c37ca193d3323
Hiago 7k <hiago7k@gmail.com>
2026-03-16 21:07:02 -0300

    usando try e catch para executar a respota da API

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿using (HttpClient client = new HttpClient()) 
 {
+
+    try 
+    {
+        string resposta = await client.GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
+        Console.WriteLine(resposta);
+    } catch(Exception ex) 
+    {
+        Console.WriteLine($"Ops acontenceu algo errado: {ex.Message}");
+    }
     
-
-
-    string resposta = await client.GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
 }

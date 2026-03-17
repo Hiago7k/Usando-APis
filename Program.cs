1ee1e760c0333e0e074d5cfc38dadca3019e5f1e
Hiago 7k <hiago7k@gmail.com>
2026-03-16 21:11:42 -0300

    iniciando teste, de try catch

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-﻿using (HttpClient client = new HttpClient()) 
-{
+﻿//using (HttpClient client = new HttpClient()) 
+//{
 
-    try 
-    {
-        string resposta = await client.GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
-        Console.WriteLine(resposta);
-    } catch(Exception ex) 
-    {
-        Console.WriteLine($"Ops acontenceu algo errado: {ex.Message}");
-    }
-    
-}
+//    try 
+//    {
+//        string resposta = await client.GetStringAsync("https://www.cheapshark.com/api/1.0/deals");
+//        Console.WriteLine(resposta);
+//    } catch(Exception ex) 
+//    {
+//        Console.WriteLine($"Ops acontenceu algo errado: {ex.Message}");
+//          //Desafio malipular essa API
+//    }
+
+//}
+
+
+// Atividade 2
+
+Console.WriteLine("Digite um numero: ");
+decimal a = Convert.ToDecimal(Console.ReadLine()!);
+Console.WriteLine("Digite outro numero: ");
+decimal b = Convert.ToDecimal(Console.ReadLine()!);
